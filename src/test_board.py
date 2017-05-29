5cc2d98afa9a1442c67068c4ef9377e71c33e5fd
Rolf Sievers <rolf.sievers@posteo.de>
2017-05-29 22:45:28 +0200

    Convert comments to docstrings after autopep8 messed them up.

--- src/test_board.py
+++ src/test_board.py
@@ -5,21 +5,21 @@ from mock import patch
 import sys
 import main
 
-# Create a board and check if the amount of cards is what we expect.
-
 
 def test_board_card_count():
+    """Create a board and check if the amount of cards is what we expect."""
+
     board = Board(4)
     count = len(board.pile.draw_pile) + len(board.pile.discard_pile)
     for hand in board.hands:
         count += sum(1 for _ in filter(lambda x: x is not None, hand))
     assert(count == 52)
 
-# Run a full example game with AIs
-# Throwing no exceptions passes the test.
-
 
 def test_full_example_run():
+    """Run a full example game with AIs
+    Throwing no exceptions passes the test."""
+
     testargs = [sys.argv[0], "Doro", "Rolf", "Judita"]
     with patch.object(sys, 'argv', testargs):
         full_example_run()
