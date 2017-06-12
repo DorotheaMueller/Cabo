2868aad3b8e437f98718c5ed9f28f6b7c5db7be9
Rolf Sievers <rolf.sievers@posteo.de>
2017-06-12 12:39:11 +0200

    Add a test for knowledge consistency

--- src/test_board.py
+++ src/test_board.py
@@ -30,3 +30,29 @@ def full_example_run():
     game = main.Game()
     game.run_subgame()
     print(game)
+
+def test_knowledge_consistency():
+    """Run a full example game with AIs
+    Throwing no exceptions passes the test."""
+
+    testargs = [sys.argv[0], "Doro", "Rolf", "Judita", "Sara"]
+    with patch.object(sys, 'argv', testargs):
+        knowledge_testing_run()
+
+def knowledge_testing_run():
+    game = main.Game()
+
+    # A modified copy of game.run_subgame()
+    game.active_player = 0
+    while game.active_player != game.board.called_cabo:
+        game.run_turn()
+        verify_knowledge_consistency(game)
+
+def verify_knowledge_consistency(game):
+    for player in game.players:
+        for player_index in range(len(player.knowledge)):
+            for card_index in range(4):
+                believe = player.knowledge[player_index][card_index]
+                truth = game.board.hands[player_index][card_index]
+                if believe != main.unknown:
+                    assert(believe == truth)
