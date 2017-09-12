ddbca1e0f493e71e12b8dd43e0a966f97ea86305
Dorothea Mueller <dorothea.mueller01@stud.uni-goettingen.de>
2017-09-12 12:21:20 +0200

    Put match method in helper

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,8 @@
+def match(user_string, string_constant, min_length=1):
+    if len(user_string.strip()) < min_length:
+        return False
+    return string_constant.lower().startswith(user_string.strip().lower())
+
 class Unknown(object):
     def __repr__(self):
         return "?"
